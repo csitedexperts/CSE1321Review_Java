696fe8492625c8ccaa10ce02ae58e6b3ce8ba9ad
ICTER Guru <icterguru@gmail.com>
2018-08-20 18:21:30 -0400

    HelloFriend added and HelloWorld updated

--- src/HellloWorld.java
+++ src/HellloWorld.java
@@ -3,9 +3,10 @@ public class HellloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello My World!");
 		System.out.println();
-		System.out.print("Welcome to Java Programming");
+		System.out.print("Welcome to Git and GitHub World");
 		System.out.println();
 		System.out.print("This is my Third Java Project");
+		System.out.print("\nI like Git and GitHub");
 		
 	}
 
