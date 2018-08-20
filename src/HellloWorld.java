aa31a64864b708ed2383a4bc356750625efd5f37
ICTER Guru <icterguru@gmail.com>
2018-08-20 17:27:57 -0400

    HelloWorld.java program is added

---
+++ src/HellloWorld.java
@@ -0,0 +1,12 @@
+public class HellloWorld {
+	
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println();
+		System.out.print("Welcome to Java Programming");
+		System.out.println();
+		System.out.print("This is my First Java Program");
+		
+	}
+
+}
