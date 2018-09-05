182f28b1a0e9f2ecd74dae5a18cabfb5df5fd6eb
Mokter Hossain <mhossa12@kennesaw.edu>
2018-09-05 14:35:11 -0400

    LeapYear program updated

--- src/control_statements/LeapYearEx1.java
+++ src/control_statements/LeapYearEx1.java
@@ -1,16 +1,20 @@
 package control_statements;
+
+
 /* LeapYearEx1.java */
 import java.io.IOException;
 import java.util.Scanner;
 public class LeapYearEx1{
 public static void main(String args[]) throws IOException { 
-	
-	int Year = 2018;
-	
-	if( ((Year%4)==0 &&(Year%100) !=0)||(Year%400)==0)
+	Scanner scnr = new Scanner(System.in);
+
+	System.out.print("Enter the year to be tested: ");
+	int Year = scnr.nextInt();
+	if( ((Year%4)==0 &&(Year%100 !=0))||(Year%400)==0)
 		System.out.println("" +Year +" is a leap year.");
 	else
 		System.out.println("" +Year +" is not a leap year.");
-	
+	scnr.close();
  }
 } 
+
